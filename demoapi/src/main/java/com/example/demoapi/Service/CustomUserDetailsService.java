98199ebdbdffa2c56e91324b7e69fb17eb3f728b
Jhovana <jhovanapatty95@gmail.com>
2026-03-03 02:35:01 -0300

    feat: implementa autenticación con Spring Security y encriptación BCrypt

---
+++ demoapi/src/main/java/com/example/demoapi/Service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.example.demoapi.Service;
+
+import com.example.demoapi.model.Usuario;
+import com.example.demoapi.Repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+
+        return new User(
+                usuario.getEmail(),
+                usuario.getPassword(),
+                Collections.emptyList()
+        );
+    }
+}
--- demoapi/src/main/java/com/example/demoapi/Service/UsuarioService.java
+++ demoapi/src/main/java/com/example/demoapi/Service/UsuarioService.java
@@ -3,6 +3,8 @@ package com.example.demoapi.Service;
 import com.example.demoapi.model.Usuario;
 import com.example.demoapi.Repository.UsuarioRepository;
 import org.springframework.stereotype.Service;
+//este import es para marcar esta clase como un servicio de Spring, lo que permite que sea inyectada en el controlador y otras partes de la aplicación
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
@@ -10,40 +12,37 @@ import java.util.List;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UsuarioService(UsuarioRepository usuarioRepository) {
+    public UsuarioService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
         this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    // GET - listar todos
     public List<Usuario> listarUsuarios() {
         return usuarioRepository.findAll();
     }
 
-    // GET por ID
     public Usuario obtenerUsuarioPorId(Long id) {
         return usuarioRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Usuario no encontrado con id: " + id));
     }
 
-    // POST - crear
     public Usuario crearUsuario(Usuario usuario) {
+        usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
         return usuarioRepository.save(usuario);
     }
 
-    // PUT - actualizar
     public Usuario actualizarUsuario(Long id, Usuario usuarioActualizado) {
         return usuarioRepository.findById(id)
                 .map(usuario -> {
                     usuario.setNombre(usuarioActualizado.getNombre());
                     usuario.setEmail(usuarioActualizado.getEmail());
-                    //usuario.setEdad(usuarioActualizado.getEdad()); // por si tienes edad
                     return usuarioRepository.save(usuario);
                 })
                 .orElseThrow(() -> new RuntimeException("Usuario no encontrado con id: " + id));
     }
 
-    // DELETE
     public void eliminarUsuario(Long id) {
         if (!usuarioRepository.existsById(id)) {
             throw new RuntimeException("Usuario no encontrado con id: " + id);
